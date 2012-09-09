0a1d498d4fdbb5c4098d6a0f3543b698128f712d
root <root@node1-1.sb8.orbit-lab.org>
2012-09-09 19:30:31 -0400

    add info about experiment setup

--- script/README.md
+++ script/README.md
@@ -3,10 +3,36 @@ The scripts in this directory are provided to make configuring and running
 GNRS-related experiments simpler and easier.
 
 ## Orbit Usage ##
+You can take a look at the following website on the basic usage of orbit:
+	http://orbit-lab.org/wiki/HowToGetStarted
+Steps to load an image:
+1. omf-5.3 tell -a offh -t TOPOLOGY
+2. omf-5.3 load -t TOPOLOGY -i IMAGE
+3. omf-5.3 tell -a offh -t TOPOLOGY
+4. omf-5.3 tell -a on -t TOPOLOGY
+
+You can ssh into each node after the node is up if the loading image succeeds.
+
+
+## Experimental Setup ##
 Below are the general steps used to configure and run an experiment on the
-Oribt Grid computers.
+Oribt Grid testbed.
 
 1. Select a topology
 1. Build the delay matrix
 1. Build the AS list.
 1. Build the delay module configuration files.
+1. Build the gnrs server and client configuration files.
+
+### delay module configuration ###
+
+
+### gnrs server configuration ###
+the gnrs server need the following configuration file:
+gnrsd.conf: contains information about server IP and port, mysql database info, etc.
+servers.lst: contains all gnrs servers IP address
+AS2node.data: the mapping from an AS num to a node ip and port
+
+### client configuration ###
+client.conf: contains info about server IP and port, client IP and port, etc.
+request.data: request datatrace.
--- script/gnrs_run_2.sh
+++ script/gnrs_run_2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#this script runs on console. It's hard coded yet for simple testing. node1-1 works as the gnrs server while node1-2 works as client.
+#it will directly calls script on each node. (omf exec sometimes doesn't work well)
 
 ssh root@node1-1 killall -9 mpstat
 ssh root@node1-1 "rm /var/log/timing*.log /var/log/*.data"
 ssh root@node1-2 "rm /var/log/timing*.log /var/log/*.data"
-#omf-5.3 exec ./gnrs_test_53_sb2.rb
 
+#configure network address and disable cpu frequency scaling
 ssh root@node1-1 "ifconfig eth0 192.168.1.110"
 ssh root@node1-2 "ifconfig eth0 192.168.1.120"
 
@@ -16,6 +18,7 @@ ssh root@node1-2 "/usr/local/mobilityfirst/code/prototype/gnrsd/scripts/cleanup_
 
 ssh root@node1-1 "mpstat -P ALL 1 > /var/log/mpstat.data 2>&1 &"
 
+#call gnrs server script and client script
 ssh root@node1-1 "/usr/local/mobilityfirst/code/prototype/gnrsd/scripts/init_gnrsd.sh gnrsd.conf 192.168.1.110 eth0 servers.lst 1 45000"
 sleep 20
 ssh root@node1-2 "/usr/local/mobilityfirst/code/prototype/gnrsd/scripts/init_gnrs_client.sh client.conf 192.168.1.120 192.168.1.110 eth0 request.data.1 10000 350"
@@ -34,6 +37,8 @@ while [ $flag -eq 1 ]; do
 done
 
 ssh root@node1-1 killall -9 mpstat
+
+#copy files from each node to console
 rm -rf ./server_stat ./client_stat
 mkdir ./server_stat ./client_stat
 scp root@node1-1:/var/log/*.data ./server_stat/
