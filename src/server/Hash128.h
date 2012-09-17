512f493c18e4fc4eba002d1e7288615b8fb64439
root <root@node1-1.sb8.orbit-lab.org>
2012-09-17 15:44:04 -0400

    modify hash interface to work for multiple copy

--- src/server/Hash128.h
+++ src/server/Hash128.h
@@ -14,9 +14,9 @@ class Hash128
 {
 	
        public: 
-		string HashG2Server(char *GUID);   //return with an server address
+		string HashG2Server(char *GUID, uint8_t hashIndex);   //return with an server address
 		uint32_t HashG2IP(char *GUID, uint8_t hashIndex);   //return with an IP address
-		uint32_t HashIP2IP(uint32_t IP);
+		uint32_t HashIP2IP(uint32_t IP, uint8_t hashIndex);
 		string MapAS2Server(asNum asNumber);   //mapping between the as# and the server IP
 	private: 
 		MD5 md5;
