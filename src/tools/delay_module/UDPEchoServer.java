f3495dcbca929bc0788f3aaee56f314c375c2fe5
Robert Moore <romoore@cs.rutgers.edu>
2012-09-17 13:11:07 -0400

    Moving packet processing to a separate thread.

--- src/tools/delay_module/UDPEchoServer.java
+++ src/tools/delay_module/UDPEchoServer.java
@@ -17,13 +17,17 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @author Robert Moore
@@ -38,10 +42,13 @@ public class UDPEchoServer {
 
     int rcvPort = Integer.parseInt(args[0]);
     final DatagramSocket rcvSocket = new DatagramSocket(rcvPort);
+    final LinkedBlockingQueue<DatagramPacket> packetQueue = new LinkedBlockingQueue<DatagramPacket>();
+    final PacketHandler worker1 = new PacketHandler(packetQueue);
+    worker1.start();
 
     Runtime.getRuntime().addShutdownHook(new Thread() {
       public void run() {
-
+        worker1.shutdown();
         rcvSocket.close();
       }
     });
@@ -50,21 +57,57 @@ public class UDPEchoServer {
     int count = 0;
     while (true) {
       rcvSocket.receive(rcvPacket);
-      System.out.println("Pkt from " + rcvPacket.getAddress() + ":" + rcvPacket.getPort());
-      byte[] data = rcvPacket.getData();
-      DataInputStream din = new DataInputStream(new ByteArrayInputStream(data));
-      int sequenceNumber = din.readInt();
-      din.close();
-      ByteArrayOutputStream bout = new ByteArrayOutputStream(8);
-      DataOutputStream dout = new DataOutputStream(bout);
-      dout.writeInt(sequenceNumber);
-      dout.writeInt(++count);
-      dout.flush();
-      data = bout.toByteArray();
-      dout.close();
-      DatagramPacket sndPacket = new DatagramPacket(data, data.length,
-          rcvPacket.getAddress(), rcvPacket.getPort());
-      rcvSocket.send(sndPacket);
+      packetQueue.put(rcvPacket);
+      rcvPacket = new DatagramPacket(new byte[8], 8);
+
+    }
+  }
+
+  private static class PacketHandler extends Thread {
+    private boolean keepRunning = true;
+    private LinkedBlockingQueue<DatagramPacket> sharedQueue;
+    private DatagramSocket sendSocket;
+
+    public PacketHandler(LinkedBlockingQueue<DatagramPacket> sharedQueue) throws SocketException {
+      super();
+      this.sharedQueue = sharedQueue;
+      this.sendSocket = new DatagramSocket();
+    }
+
+    public void shutdown() {
+      this.keepRunning = false;
+      this.interrupt();
+    }
+
+    public void run() {
+      DatagramPacket rcvPacket;
+      while (this.keepRunning) {
+        try {
+          rcvPacket = this.sharedQueue.take();
+          System.out.println("Pkt from " + rcvPacket.getAddress() + ":"
+              + rcvPacket.getPort());
+          byte[] data = rcvPacket.getData();
+          DataInputStream din = new DataInputStream(new ByteArrayInputStream(
+              data));
+          int sequenceNumber = din.readInt();
+          din.close();
+          ByteArrayOutputStream bout = new ByteArrayOutputStream(8);
+          DataOutputStream dout = new DataOutputStream(bout);
+          dout.writeInt(sequenceNumber);
+          dout.writeInt(1);
+          dout.flush();
+          data = bout.toByteArray();
+          dout.close();
+          DatagramPacket sndPacket = new DatagramPacket(data, data.length,
+              rcvPacket.getAddress(), rcvPacket.getPort());
+          this.sendSocket.send(sndPacket);
+        } catch (InterruptedException ie) {
+          // Ignored
+        }
+        catch(Exception e){
+          e.printStackTrace();
+        }
+      }
     }
   }
 }
