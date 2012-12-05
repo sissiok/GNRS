ba858f9f73bb8f507517dbf5e55121a0fe7f5023
Robert Moore <romoore@cs.rutgers.edu>
2012-12-05 13:38:25 -0500

    Correctly reusing UDP port.

--- jserver/src/main/java/edu/rutgers/winlab/mfirst/net/ipv4udp/IPv4UDPNAO.java
+++ jserver/src/main/java/edu/rutgers/winlab/mfirst/net/ipv4udp/IPv4UDPNAO.java
@@ -75,7 +75,9 @@ import edu.rutgers.winlab.mfirst.net.SessionParameters;
  * @author Robert Moore
  */
 public class IPv4UDPNAO extends IoHandlerAdapter implements
-    NetworkAccessObject, IoFutureListener<ConnectFuture> {
+    NetworkAccessObject 
+//    ,IoFutureListener<ConnectFuture> 
+    {
 
   /**
    * Logging for this class.
@@ -99,7 +101,7 @@ public class IPv4UDPNAO extends IoHandlerAdapter implements
   /**
    * Messages that are awaiting connections.
    */
-  private final transient Map<ConnectFuture, RelayInfo> awaitingConnect = new ConcurrentHashMap<ConnectFuture, RelayInfo>();
+//  private final transient Map<ConnectFuture, RelayInfo> awaitingConnect = new ConcurrentHashMap<ConnectFuture, RelayInfo>();
 
   /**
    * Incoming datagram acceptor.
@@ -109,13 +111,15 @@ public class IPv4UDPNAO extends IoHandlerAdapter implements
   /**
    * For sending datagrams.
    */
-  private final transient NioDatagramConnector connector;
+//  private final transient NioDatagramConnector connector;
 
   private final transient IPv4UDPAddress listenAddress;
+  
+  private final transient InetSocketAddress listenSockAddr;
 
-  private final transient IPv4UDPAddress sendAddress;
-
-  private final transient InetSocketAddress sendSockAddr;
+//  private final transient IPv4UDPAddress sendAddress;
+//
+//  private final transient InetSocketAddress sendSockAddr;
 
   /**
    * Creates a new instance of network access object for IPv4/UDP networking.
@@ -134,17 +138,20 @@ public class IPv4UDPNAO extends IoHandlerAdapter implements
     }
     this.listenAddress = IPv4UDPAddress.fromASCII(this.config.getBindAddress()
         + ":" + this.config.getBindPort());
+    
+    this.listenSockAddr = new InetSocketAddress(this.config.getBindAddress(),
+        this.config.getBindPort());
 
-    this.sendAddress = IPv4UDPAddress.fromASCII(this.config.getBindAddress()
-        + ":" + this.config.getSendPort());
-    this.sendSockAddr = IPv4UDPAddress.toSocketAddr(this.sendAddress);
+//    this.sendAddress = IPv4UDPAddress.fromASCII(this.config.getBindAddress()
+//        + ":" + this.config.getSendPort());
+//    this.sendSockAddr = IPv4UDPAddress.toSocketAddr(this.sendAddress);
 
     // Incoming messages (and direct replies)
     this.acceptor = new NioDatagramAcceptor();
     this.configureAcceptor();
     // Outgoing messages
-    this.connector = new NioDatagramConnector();
-    this.configureConnector();
+//    this.connector = new NioDatagramConnector();
+//    this.configureConnector();
 
   }
 
@@ -171,8 +178,7 @@ public class IPv4UDPNAO extends IoHandlerAdapter implements
     }
     // Bind to a specific IP address.
     else {
-      this.acceptor.bind(new InetSocketAddress(this.config.getBindAddress(),
-          this.config.getBindPort()));
+      this.acceptor.bind(this.listenSockAddr);
     }
   }
 
@@ -181,19 +187,19 @@ public class IPv4UDPNAO extends IoHandlerAdapter implements
    * 
    * @throws IOException
    */
-  private void configureConnector() throws IOException {
-    this.connector.setHandler(this);
-    final DatagramSessionConfig sessionConfig = this.connector
-        .getSessionConfig();
-    sessionConfig.setReuseAddress(true);
-    sessionConfig.setCloseOnPortUnreachable(false);
-    sessionConfig.setIdleTime(IdleStatus.BOTH_IDLE, 1);
-    sessionConfig.setIdleTime(IdleStatus.BOTH_IDLE, 1);
-
-    final DefaultIoFilterChainBuilder chain = this.connector.getFilterChain();
-    chain.addLast("gnrs codec", new ProtocolCodecFilter(
-        new GNRSProtocolCodecFactory()));
-  }
+//  private void configureConnector() throws IOException {
+//    this.connector.setHandler(this);
+//    final DatagramSessionConfig sessionConfig = this.connector
+//        .getSessionConfig();
+//    sessionConfig.setReuseAddress(true);
+//    sessionConfig.setCloseOnPortUnreachable(false);
+//    sessionConfig.setIdleTime(IdleStatus.BOTH_IDLE, 1);
+//    sessionConfig.setIdleTime(IdleStatus.BOTH_IDLE, 1);
+//
+//    final DefaultIoFilterChainBuilder chain = this.connector.getFilterChain();
+//    chain.addLast("gnrs codec", new ProtocolCodecFilter(
+//        new GNRSProtocolCodecFactory()));
+//  }
 
   /**
    * Loads this NAOs configuration file from the filename provided.
@@ -237,24 +243,27 @@ public class IPv4UDPNAO extends IoHandlerAdapter implements
       IPv4UDPParameters params = this.connections.get(destAddr);
 
       if (params == null) {
+        params = new IPv4UDPParameters();
+         params.session = this.acceptor.newSession(IPv4UDPAddress.toSocketAddr(destAddr), this.listenSockAddr);
+        this.connections.put(destAddr,params);
         // LOG.info("Establishing connection to {}",
         // IPv4UDPAddress.toSocketAddr(destAddr));
-        final ConnectFuture future = this.connector.connect(
-            IPv4UDPAddress.toSocketAddr(destAddr), this.sendSockAddr);
-        RelayInfo info = new RelayInfo();
-        info.clientMessage = message;
-        info.remoteAddress = destAddr;
-        this.awaitingConnect.put(future, info);
-        future.addListener(this);
+//        final ConnectFuture future = this.connector.connect(
+//            IPv4UDPAddress.toSocketAddr(destAddr), this.listenSockAddr);
+//        RelayInfo info = new RelayInfo();
+//        info.clientMessage = message;
+//        info.remoteAddress = destAddr;
+//        this.awaitingConnect.put(future, info);
+//        future.addListener(this);
         // FIXME: Must call awaitUninterruptably. This is a known issue in MINA
         // <https://issues.apache.org/jira/browse/DIRMINA-911>
-        future.awaitUninterruptibly();
+//        future.awaitUninterruptibly();
       }
       // Have an existing connection
-      else {
+//      else {
 
         this.actualSend(params, message);
-      }
+//      }
     }
 
   }
@@ -344,19 +353,19 @@ public class IPv4UDPNAO extends IoHandlerAdapter implements
     this.acceptor.dispose(true);
   }
 
-  @Override
-  public void operationComplete(ConnectFuture future) {
-    RelayInfo info = this.awaitingConnect.remove(future);
-    if (info == null) {
-      LOG.error("Unable to find correct message for {}", future);
-    } else {
-      IPv4UDPParameters params = new IPv4UDPParameters();
-      params.session = future.getSession();
-      info.serverParams = params;
-      this.connections.put(info.remoteAddress, info.serverParams);
-      this.sessions.put(params.session, info.remoteAddress);
-      this.sendMessage(info.clientMessage, info.remoteAddress);
-    }
-  }
+//  @Override
+//  public void operationComplete(ConnectFuture future) {
+//    RelayInfo info = this.awaitingConnect.remove(future);
+//    if (info == null) {
+//      LOG.error("Unable to find correct message for {}", future);
+//    } else {
+//      IPv4UDPParameters params = new IPv4UDPParameters();
+//      params.session = future.getSession();
+//      info.serverParams = params;
+//      this.connections.put(info.remoteAddress, info.serverParams);
+//      this.sessions.put(params.session, info.remoteAddress);
+//      this.sendMessage(info.clientMessage, info.remoteAddress);
+//    }
+//  }
 
 }
