495c5bb4bd016850d4497a7ab74af56d35bafbec
Robert Moore <romoore@cs.rutgers.edu>
2013-01-10 13:13:51 -0500

    Removing backticks BLARGH!

--- omf/prepare.sh
+++ omf/prepare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Data files
+
+export PATH="$PATH":.
+
 BASE_FILE="topology"
 TOPO_FILE="$BASE_FILE.data"
 PREFIX_FILE="prefix.data"
@@ -20,19 +23,19 @@ if [ ${ARCH} == 'x86_64' ]; then
 fi
 
 echo "Downloading required files"
-`$WGET $DWNLD_URL$SPG`
+$WGET $DWNLD_URL$SPG
 chmod +x $SPG
-`$WGET $SCRIPT_URL$AS_UNIQ`
+$WGET $SCRIPT_URL$AS_UNIQ
 chmod +x $AS_UNIQ
-`$WGET $SCRIPT_URL$DEL_GEN_CLT`
+$WGET $SCRIPT_URL$DEL_GEN_CLT
 chmod +x $DEL_GEN_CLT
-`$WGET $SCRIPT_URL$DEL_GEN_SRV`
+$WGET $SCRIPT_URL$DEL_GEN_SRV
 chmod +x $DEL_GEN_SRV
 
 echo "Generating files"
-`./$SPG $TOPO_FILE`
+$SPG $TOPO_FILE
 rm AS_arr.data
-`./$AS_UNIQ $TOPO_FILE $AS_FILE`
-`./$DEL_GEN_SRV $AS_FILE $ROUTE_FILE`
-`./$DEL_GEN_CLT $AS_FILE $ROUTE_FILE`
+$AS_UNIQ $TOPO_FILE $AS_FILE
+$DEL_GEN_SRV $AS_FILE $ROUTE_FILE
+$DEL_GEN_CLT $AS_FILE $ROUTE_FILE
 
