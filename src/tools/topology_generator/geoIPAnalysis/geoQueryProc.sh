a58907d2637fa2011a4daeeddddc4633243bed8d
Feixiong Zhang <alexzhang012@gmail.com>
2013-01-14 14:28:46 -0500

    change geoqueryresult download URL

--- src/tools/topology_generator/geoIPAnalysis/geoQueryProc.sh
+++ src/tools/topology_generator/geoIPAnalysis/geoQueryProc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # this script is used to process the result after getting the geoQueryResult from the script ./geoQuery.sh
 
-wget https://bitbucket.org/romoore/gnrs/downloads/geoQueryResult
+wget http://winlab.rutgers.edu/~feixiong/geoQueryResult
+#wget https://bitbucket.org/romoore/gnrs/downloads/geoQueryResult
 wget https://bitbucket.org/romoore/gnrs/downloads/AS.data
 
 ./geoQueryProc.pl
