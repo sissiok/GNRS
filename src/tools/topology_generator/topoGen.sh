f4b6ea94b975f9088c935281ec1572a4e4fe25f4
Robert Moore <romoore@cs.rutgers.edu>
2013-01-16 12:34:04 -0500

    Cleaning-up the script a bit.

--- src/tools/topology_generator/topoGen.sh
+++ src/tools/topology_generator/topoGen.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-wget https://bitbucket.org/romoore/gnrs/downloads/ASPrefixData.mat
-wget https://bitbucket.org/romoore/gnrs/downloads/topologyData.mat
-wget https://bitbucket.org/romoore/gnrs/downloads/shellMem.mat
-wget https://bitbucket.org/romoore/gnrs/downloads/hangMem.mat
-wget https://bitbucket.org/romoore/gnrs/downloads/clique.mat
-wget https://bitbucket.org/romoore/gnrs/downloads/linkPrefixofLOC.mat
+PATH="$PATH":.
 
-./foo.sh $1 $2
+BASE_URL="https://bitbucket.org/romoore/gnrs/downloads/"
+WGET="wget --timeout=10 -N"
+REAL_SCRIPT="foo.sh"
+
+PREFIX_DATA="ASPrefixData.mat"
+TOPO_DATA="topologyData.mat"
+SHELL_DATA="shellMem.mat"
+HANG_DATA="hangMem.mat"
+CLIQUE_DATA="clique.mat"
+LINK_PREFIX_DATA="linkPrefixofLOC.mat"
+
+DOWNLOADED=(	"$PREFIX_DATA" "$TOPO_DATA" "$SHELL_DATA" \
+		"$HANG_DATA" "$CLIQUE_DATA" "$LINK_PREFIX_DATA");
+
+# Download the required files
+for I in "${DOWNLOADED[@]}"; do
+	echo "$WGET $BASE_URL$I"
+	$WGET $BASE_URL$I || exit 1;
+done
+# Exec the actual script
+$REAL_SCRIPT $1 $2
+
+for I in "${DOWNLOADED[@]}"; do
+	rm $I;
+done
