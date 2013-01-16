f7a10831159026531de1735995908ba6b543ee84
Alex Zhang <alexzhang012@gmail.com>
2013-01-15 19:11:29 -0500

    modify sizeTopoGenerator to ensure connectivity

--- src/tools/topology_generator/sizeTopoGenerator.m
+++ src/tools/topology_generator/sizeTopoGenerator.m
@@ -1,7 +1,6 @@
 function [ asID, asLinks, asPrefix ] = sizeTopoGenerator( asNum )
 % Analyse Number of /32 Addresses in each AS, extract the largest 200 ASes,
 % and generate the topology    
-
 %inputFile = csvread('ASNumDataMod-2_1_11.csv');
 load ASPrefixData;
 numPrefix = length(inputFile);
@@ -21,12 +20,45 @@ ASsizeInfo = [find(addrCount>0) addrCount(find(addrCount>0))];
 ASsizeInfo = sortrows(ASsizeInfo, -2);
 asID = ASsizeInfo(1:asNum,1);
 
-%prefixFile = fopen('prefix_2_1_11_adjusted.data');
+
+%find links among those asNum ASes
+%topologyData = importdata('topology_intra_adjusted.data');
+load topologyData;
+ASThreshold = asNum;
+flag=1;
+
+while(flag==1)
+    asLinks = zeros(0,3);
+    linkNum = 0;
+
+    for i= 1:length(topologyData)
+        if(isempty(find(asID==topologyData(i,1)))==0 && isempty(find(asID==topologyData(i,2)))==0)
+            linkNum = linkNum +1;
+            asLinks(linkNum,:) = topologyData(i,:);
+        end
+    end            
+
+    %count linkNumPerAS to avoid any AS with 0 degree
+    linkNumPerAS = zeros(asNum,1);
+    for i=1:asNum
+        linkNumPerAS(i) = length(find(asLinks(:,1:2)==asID(i)));
+    end
+
+    temp = find(linkNumPerAS==0);
+    if(isempty(temp)==1)
+        flag=0;
+    else
+        %replace those ASes that are isolated (0 degree)
+        for j=1:length(temp)
+            asID(temp(j)) = ASsizeInfo(ASThreshold+j,1);
+        end
+        ASThreshold = ASThreshold + length(temp);
+    end
+end
+
+% extract those prefixes of asID from the prefix file
 asPrefix = zeros(0,6);
 asPrefixNum = 0;
-
-%linedata = sscanf(fgetl(prefixFile), '%s %d');
-%fscan(prefixFile, '%s %d', [prefix ASid]);
 
 for i = 1:numPrefix
     if(isempty(find(asID==inputFile(i,1)))==0)
@@ -35,18 +67,4 @@ for i = 1:numPrefix
     end
 end
 
-%find links among those asNum ASes
-%topologyData = importdata('topology_intra_adjusted.data');
-load topologyData;
-asLinks = zeros(0,3);
-linkNum = 0;
-
-for i= 1:length(topologyData)
-    if(isempty(find(asID==topologyData(i,1)))==0 && isempty(find(asID==topologyData(i,2)))==0)
-        linkNum = linkNum +1;
-        asLinks(linkNum,:) = topologyData(i,:);
-    end
-end            
-
 end
-
