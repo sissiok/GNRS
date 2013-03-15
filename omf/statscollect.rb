152bd2393a3f84d72c36337060d3a376b243ac32
Robert Moore <romoore@cs.rutgers.edu>
2013-03-14 20:46:34 -0400

    Fixing typo.

--- omf/statscollect.rb
+++ omf/statscollect.rb
@@ -18,16 +18,17 @@ def collectClientStats(clientsMap, prefixDir)
 	asCount = Hash.new(0)
 	clientsMap.each_value { |group|
 		group.nodelist.each { |client|
-			path = prefixDir.empty? ? "client_#{client.asNumber}/" : "#{prefixDir}/client_#{client.asNumber}/";
-			if File.exists?(path)  and File.directory?(path)
-				nextId = uniqueId[path]
-				uniqueId[path] = nextId+1
-				path = path[0..-2]
-				path = "#{path}_#{nextId}/"
-			end
+			path = prefixDir.empty? ? "client_#{client.asNumber}R#{asCount[client.asNumber]}/" : "#{prefixDir}/client_#{client.asNumber}R#{asCount[client.asNumber]}/";
+			
+			#if File.exists?(path)  and File.directory?(path)
+			#	nextId = uniqueId[path]
+			#	uniqueId[path] = nextId+1
+			#	path = path[0..-2]
+			#	path = "#{path}_#{nextId}/"
+			#end
 			system("mkdir -p #{path}");
-			system("#{property.scp} root@#{group.hostname}:\"/var/gnrs/stats#{client.asNumber}R#{asCount[client.asNumber}/*\" #{path}");
-			system("#{property.scp} root@#{group.hostname}:/var/gnrs/client#{client.asNumber}.log #{path}");
+			system("#{property.scp} root@#{group.hostname}:\"/var/gnrs/stats#{client.asNumber}R#{asCount[client.asNumber]}/*\" #{path}");
+			#system("#{property.scp} root@#{group.hostname}:/var/gnrs/client#{client.asNumber}.log #{path}");
 			asCount[client.asNumber] = asCount[client.asNumber] + 1
 
 			# exitStatus = $?.exitstatus
