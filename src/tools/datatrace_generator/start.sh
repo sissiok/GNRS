f9a8cf777c93bd14a61fdef6a301cd3e91d1598d
Feixiong Zhang <alexzhang012@gmail.com>
2013-03-28 11:22:43 -0400

    add more comment to start.sh

--- src/tools/datatrace_generator/start.sh
+++ src/tools/datatrace_generator/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/octave -qf
 
 #input: .mat file containing topology data and prefix data which is generated from topology generator
+#example: ./start.sh ../topology_generator/datatraceInput.mat
 
 arg_list = argv ();
 matFile = arg_list{1};
